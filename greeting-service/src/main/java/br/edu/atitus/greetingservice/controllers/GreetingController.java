12596d3ea13c0d0038d4f7986f0f74fe55afe42d
lucianoferretto <prof.ferretto@yahoo.com.br>
2026-04-10 15:13:20 -0300

    Criação do Projeto greeting-service e configuração da rota inicial

---
+++ greeting-service/src/main/java/br/edu/atitus/greetingservice/controllers/GreetingController.java
@@ -0,0 +1,22 @@
+package br.edu.atitus.greetingservice.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping({"", "/", "/{namePath}"})
+    public ResponseEntity<String> getGreeting(
+            @RequestParam(required = false) String name,
+            @PathVariable(required = false) String namePath
+    ) {
+        if (name == null) {
+            name = namePath != null ? namePath : "World";
+        }
+
+        String retorno = String.format("%s %s!!!", "Hello", name);
+        return ResponseEntity.ok(retorno);
+    }
+}
